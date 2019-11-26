868bd962b94ad6a72065df68212de0ee25e02edc
drthisguy <page.c.tyler@gmail.com>
2019-11-25 23:56:57 -0500

    layout changes. Set variable needed in app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Generators
 
 function getUpper() {
-    var arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function getLower() {
-    var arr = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    return arr[Math.floor(Math.random() * arr.length)];
+    return getUpper().toLowerCase();
 }
 
 function getNumber() {
@@ -16,8 +15,13 @@ function getNumber() {
 }
 
 function getSpecialChar() {
-    var arr = [' ','!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '}', '|', '}', '~'];
+    var arr = [' ','!', '"','#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '}', '|', '}', '~'];
     return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getLength() {
+    var length = document.querySelector('.length');
+    return parseInt(length.value);
 }
 console.log(getUpper());
 console.log(getLower());
@@ -26,19 +30,32 @@ console.log(getSpecialChar());
 
 //Get input values from document
 var docGenerate = document.querySelector('.generate');
-// var docLength = document.querySelector('.length');
+
+var docLength = document.querySelector('.length-box');
 var docUpCase = document.querySelector('.alpha-upp');
 var docLowCase = document.querySelector('.alpha-low');
 var docNumeric = document.querySelector('.numeric');
 var docSpecial = document.querySelector('.special');
 
 docGenerate.addEventListener('click', function() {
-    var length = document.querySelector('.length');
-    length = parseInt(length.value);
+    
+    var needsLength = docUpCase.checked;
+    var length = needsLength ? getLength() : false;
     console.log(length);
+    var needsUpper = docUpCase.checked;
+    var upper = needsUpper ? getUpper() : false;
+    console.log(upper);
+    var needsLower = docLowCase.checked;
+    var lower = needsLower ? getLower() : false;
+    console.log(lower);
+    var needsNumber = docNumeric.checked;
+    var numeric = needsNumber ? getNumber() : false;
+    console.log(numeric);
+    var needsSpecial = docUpCase.checked;
+    var special = needsSpecial ? getSpecialChar() : false;
+    console.log(special);
 
-    var upperCase = docUpCase.checked;
-    console.log(upperCase);
+    
 })  
 
 
