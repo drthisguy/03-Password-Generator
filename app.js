b17cc1cb547565ca2685c70c3ac469619352ee18
drthisguy <page.c.tyler@gmail.com>
2019-11-27 21:47:26 -0500

    completely rebuilt app.js and have a working generator

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ function setRandomLength(min, max) {
     return Math.floor((Math.random() * (max - min) + min));
 }
 
+function randomizor(arr) {
+    //pick random array
+    randomArray =  arr[Math.floor(Math.random() * arr.length)];
+
+    //return random character from random array
+    return randomArray[Math.floor(Math.random() * randomArray.length)]
+}
+
 //return length range values.
 function getLength() {
     
@@ -20,82 +28,70 @@ function getLength() {
 
       }else if (isNaN(max) && Number.isInteger(min)) {
           
-            return min;
+          return min;
 
     }else {
         return setRandomLength(min, max);
     }
 }
 
-//generates random upppercase
-function getUpper() {
-    var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-//generates random lowercase
-function getLower() {
-    return getUpper().toLowerCase();
-}
-//generates random number
-function getNumber() {
-    arr = ['0','1', '2','3','4','5','6','7','8','9'];
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-//generates random special char
-function getSpecialChar() {
-    var arr = [' ','!', '"','#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '}', '|', '}', '~'];
-    return arr[Math.floor(Math.random() * arr.length)];
-}
+//sort out unchecked criteria.
+function checkBox(arr) {
 
-function randomizor(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-
-function checkBox(oldArr) {
-    var newArr = [];
-    for (var i=0; i< oldArr.length; i++)
-        if (oldArr[i] !== false) {
-            newArr.push(oldArr[i]);
+    var criteria = [];
+    for (var i=0; i < arr.length; i++)
+        if (arr[i] !== false) {
+            criteria.push(arr[i]);
         }
-    return newArr;
+    return criteria;
 }
+
+// toLowerCase for an array.
+function lowerCase(alphabet) {
+    return alphabet.toLowerCase();
+}
+
+// Criteria arrays
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lower = upper.map(lowerCase);
+var numeric = ['0','1', '2','3','4','5','6','7','8','9'];
+var special = [' ','!', '"','#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '}', '|', '}', '~'];
+
 var generator = document.querySelector('.generate');
 
 generator.addEventListener('click', function() {
 
     /*Get and set all input parameters from the dom*/
-    var charz = [];
     
     var needsLength = document.querySelector('.length').checked;
-    var length = needsLength ? getLength() : setRandomLength(8, 128); //default range for length.
+    length = needsLength ? getLength() : setRandomLength(8, 128); //default range for length.
     
     var needsUpper =  document.querySelector('.alpha-upp').checked;
-    var upper = needsUpper ? getUpper() : false;
+    upper = needsUpper ? upper : false;
     
     var needsLower =  document.querySelector('.alpha-low').checked;
-    var lower = needsLower ? getLower() :needsLower ? charz.push(lower) : false;
+    lower = needsLower ? lower : false;
    
     var needsNumber = document.querySelector('.numeric').checked;
-    var numeric = needsNumber ? getNumber() : needsNumber ? charz.push(numeric) : false;
+    numeric = needsNumber ? numeric : false;
   
     var needsSpecial = document.querySelector('.special').checked;
-    var special = needsSpecial ? getSpecialChar() : needsSpecial ? charz.push(special) : false;
+    special = needsSpecial ? special : false;
     
-    console.log(charz, length, upper, lower, numeric, special);
+    console.log(length, upper, lower, numeric, special);
     
     var charz = [upper, lower, numeric, special];
-    charz = checkBox(charz); 
+    charz = checkBox(charz); // sort out unchecked criteria
 
 
-
-    // charz = randomizor(charz);
     console.log(charz);
-    // var output = '';
-    // for ( let i = 0; i <= length; i++ ) {
-    //     output += charz[i];
-    //     console.log(output);
-    // }
-    
+    var output = '';
+    for ( let i = 0; i <= length; i++ ) {
+        
+        output += randomizor(charz);
+        console.log(output);
+    }
+    document.getElementById('output').innerHTML = output;
 })  
 
 
