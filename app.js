0f256e364414624b04ee7873443e645c7aed8891
My Lord <paget@sapling-inc.local>
2019-12-04 14:39:04 -0500

    removed style.css (not used). typed up README.md and polished up index.html and app.js.

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-/*                  Functions                   */
-
 function randomizor(arr) {
-  //pick random array
-  randomArray = arr[Math.floor(Math.random() * arr.length)];
+  //pick out random array
+  arr = arr[Math.floor(Math.random() * arr.length)];
 
   //return random character from random array
-  return randomArray[Math.floor(Math.random() * randomArray.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//return length range values.
+//return password length.
 function getLength() {
   var min = parseInt(document.querySelector(".min").value);
   var max = parseInt(document.querySelector(".max").value);
 
   if (isNaN(min) && isNaN(max)) {
     //validate.
-    alert('Your password length criteria is not valid.');
+    alert("Your password length criteria is not valid.");
   } else if (isNaN(min) && Number.isInteger(max)) {
     return max;
   } else if (isNaN(max) && Number.isInteger(min)) {
@@ -57,24 +55,20 @@ function lowerCase(alphabet) {
   return alphabet.toLowerCase();
 }
 
+// enable/disable text fields for length.
+document.querySelector(".length").onchange = function() {
+  document.querySelector(".min").disabled = !this.checked;
+  document.querySelector(".max").disabled = !this.checked;
+};
+
 // Criteria arrays
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lower = upper.map(lowerCase);
 var numeric = ['0','1', '2','3','4','5','6','7','8','9'];
 var special = [' ','!', '"','#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[','\\', ']', '^', '_', '`', '}', '|', '}', '~'];
 
-// enable/disable text fields for PW range. 
-document.querySelector('.length').onchange = function() {
-    document.querySelector('.min').disabled = !this.checked;
-    document.querySelector('.max').disabled = !this.checked;
-};
-
-function hideOutput() {
-    document.getElementById('output').innerHTML = '';
-};
 
 document.querySelector(".generate").addEventListener("click", function() {
-    hideOutput();
   /*Get and set all input parameters from the dom*/
 
   var needsLength = document.querySelector(".length").checked;
@@ -93,27 +87,25 @@ document.querySelector(".generate").addEventListener("click", function() {
   special = needsSpecial ? special : false;
 
   var charz = [upper, lower, numeric, special];
+
+  //sort out unchecked criteria
   charz = checkBox(charz);
 
-  console.log(charz);
+  //generate password
   var output = "";
   for (var i = 0; i <= length; i++) {
     output += randomizor(charz);
   }
 
   if (output !== "") {
-
-  document.getElementById(
-    "output"
-  ).innerHTML = `<p class = "text-break" id="clipboard">${output}</p>  
+    document.getElementById(
+      "output"
+    ).innerHTML = `<p class = "text-break" id="clipboard">${output}</p>  
     <div class="row"><div class="col"><button type="button" class="btn btn-success mt-4 copy">Copy to Clipboard</button></div></div>`;
-  
-  //Copy password to clipboard.
-  document.querySelector(".copy").addEventListener("click", copier);
+
+    //Copy password to clipboard.
+    document.querySelector(".copy").addEventListener("click", copier);
+  } else {
+    document.getElementById("output").textContent = "";
   }
-     else {
-        hideOutput();
-     }
 });
-
-
